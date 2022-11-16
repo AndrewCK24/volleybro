5159d10960a216d167a308fdd6a09c17898303b3
AndrewCK24 <andrew50412@gmail.com>
2022-11-17 03:27:10 +0800

    Update controller.py

--- pythonVer/controller.py
+++ pythonVer/controller.py
@@ -7,16 +7,22 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setup_control()
+        #TODO: change back to landingPage
+        # 可將以下含式改為包含載入所有login後所需之資訊  # self.login()
         self.ui.outerStack.setCurrentWidget(self.ui.mainPage) # 設定程式進入畫面
 
     def setup_control(self):
         """teamPage"""
         self.ui.browseBenchBtn.clicked.connect(self.browseBench)
         self.ui.editRegularBtn.clicked.connect(self.editRegular)
+        """regularEditPage"""
+        #TODO: to make sure that there are [regularPage, regularEditPage, benchPage, benchEditPage]
 
+    # teamPage
     def browseBench(self):
         print("browseBench started")
-    
+        print("browseBench finished")
     def editRegular(self):
         print("editRegular started")
         self.ui.mainStack.setCurrentWidget(self.ui.teamEditPage)
+        print("editRegular finished")
