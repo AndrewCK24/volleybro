995573925440cb3f846da29ab8850bc54e564b68
AndrewCK24 <andrew50412@gmail.com>
2022-11-17 03:09:30 +0800

    update controller

--- pythonVer/controller.py
+++ pythonVer/controller.py
@@ -7,7 +7,16 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setup_control()
-        self.ui.stackedWidget.setCurrentWidget(self.ui.landingPage) # 設定程式進入畫面
+        self.ui.outerStack.setCurrentWidget(self.ui.mainPage) # 設定程式進入畫面
 
     def setup_control(self):
-        """mainPage"""
+        """teamPage"""
+        self.ui.browseBenchBtn.clicked.connect(self.browseBench)
+        self.ui.editRegularBtn.clicked.connect(self.editRegular)
+
+    def browseBench(self):
+        print("browseBench started")
+    
+    def editRegular(self):
+        print("editRegular started")
+        self.ui.mainStack.setCurrentWidget(self.ui.teamEditPage)
