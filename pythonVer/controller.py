1bfbb780130197276fd6c1e52e2e71010a422909
AndrewCK24 <andrew50412@gmail.com>
2022-11-17 17:11:55 +0800

    update btn function

--- pythonVer/controller.py
+++ pythonVer/controller.py
@@ -7,26 +7,39 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setup_control()
-        #TODO: change back to landingPage
-        # 可將以下含式改為包含載入所有login後所需之資訊  # self.login()
-        self.ui.outerStack.setCurrentWidget(self.ui.mainPage) # 設定程式進入畫面
-        self.ui.mainStack.setCurrentWidget(self.ui.memberRegularPage)
+        self.initialize_homepages()
 
     # 建立按鈕功能
     def setup_control(self):
         """navBar"""
+        self.ui.dashboardBtn.clicked.connect(self.show_dashboard)
         self.ui.rosterBtn.clicked.connect(self.show_regularPage)
-        self.ui.newGameBtn.clicked.connect(self.show_gameEdit)
-        ### """MemberPages""" ###
-        """regularPage"""
+        self.ui.historyBtn.clicked.connect(self.show_historyPage)
+        self.ui.newGameBtn.clicked.connect(self.create_new_record)
+        ### """rosterPages""" ###
+        """regularPage, regularEditPage"""
         self.ui.browseBenchBtn.clicked.connect(self.show_benchPage)
         self.ui.editRegularBtn.clicked.connect(self.show_regularEdit)
-        """regularEditPage"""
         self.ui.saveRegularBtn.clicked.connect(self.show_regularPage)
+        """benchPage, benchEditPage"""
+        self.ui.browseRegularBtn.clicked.connect(self.show_regularPage)
+        self.ui.editBenchBtn.clicked.connect(self.show_benchEdit)
+        self.ui.saveBenchBtn.clicked.connect(self.show_benchPage)
         #TODO: to make sure that there are [regularPage, regularEditPage, benchPage, benchEditPage]
 
-    # 檢查球員檔案
+    """球員檔案載入、編輯"""
+    # def 
     
+    """頁面切換"""
+    def initialize_homepages(self):
+        #TODO: change back to landingPage
+        # 可將以下含式改為包含載入所有login後所需之資訊  # self.login()
+        self.ui.outerStack.setCurrentWidget(self.ui.mainPage) # 設定程式進入畫面
+        self.ui.mainStack.setCurrentWidget(self.ui.memberRegularPage)
+    def show_dashboard(self):
+        self.ui.mainStack.setCurrentWidget(self.ui.dashboardPage)
+    def show_historyPage(self):
+        self.ui.mainStack.setCurrentWidget(self.ui.historyPage)
     # memberPages
     def show_regularPage(self):
         self.ui.mainStack.setCurrentWidget(self.ui.memberRegularPage)
@@ -35,10 +48,14 @@ class MainWindow_controller(QtWidgets.QMainWindow):
     def show_regularEdit(self):
         self.ui.mainStack.setCurrentWidget(self.ui.memberRegularEditPage)
     def show_benchEdit(self):
-        self.ui.mainStack.setCurrentWidget(self.ui.memberBenchPage)
+        self.ui.mainStack.setCurrentWidget(self.ui.memberBenchEditPage)
+    
+    """賽事記錄"""
     # gameEditPage
     def create_new_record(self):
         #TODO: 創建新的比賽檔案
         self.show_gameEdit()
     def show_gameEdit(self):
         self.ui.mainStack.setCurrentWidget(self.ui.gameEditPage)
+
+    """"""
