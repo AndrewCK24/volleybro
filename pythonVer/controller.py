69ced2623585c5a8dff9acceaf064a75672eae4b
AndrewCK24 <andrew50412@gmail.com>
2022-11-17 14:06:54 +0800

    update memberPages and switch functions

--- pythonVer/controller.py
+++ pythonVer/controller.py
@@ -10,19 +10,35 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         #TODO: change back to landingPage
         # 可將以下含式改為包含載入所有login後所需之資訊  # self.login()
         self.ui.outerStack.setCurrentWidget(self.ui.mainPage) # 設定程式進入畫面
+        self.ui.mainStack.setCurrentWidget(self.ui.memberRegularPage)
 
+    # 建立按鈕功能
     def setup_control(self):
-        """teamPage"""
-        self.ui.browseBenchBtn.clicked.connect(self.browseBench)
-        self.ui.editRegularBtn.clicked.connect(self.editRegular)
+        """navBar"""
+        self.ui.rosterBtn.clicked.connect(self.show_regularPage)
+        self.ui.newGameBtn.clicked.connect(self.show_gameEdit)
+        ### """MemberPages""" ###
+        """regularPage"""
+        self.ui.browseBenchBtn.clicked.connect(self.show_benchPage)
+        self.ui.editRegularBtn.clicked.connect(self.show_regularEdit)
         """regularEditPage"""
+        self.ui.saveRegularBtn.clicked.connect(self.show_regularPage)
         #TODO: to make sure that there are [regularPage, regularEditPage, benchPage, benchEditPage]
 
-    # teamPage
-    def browseBench(self):
-        print("browseBench started")
-        print("browseBench finished")
-    def editRegular(self):
-        print("editRegular started")
-        self.ui.mainStack.setCurrentWidget(self.ui.teamEditPage)
-        print("editRegular finished")
+    # 檢查球員檔案
+    
+    # memberPages
+    def show_regularPage(self):
+        self.ui.mainStack.setCurrentWidget(self.ui.memberRegularPage)
+    def show_benchPage(self):
+        self.ui.mainStack.setCurrentWidget(self.ui.memberBenchPage)
+    def show_regularEdit(self):
+        self.ui.mainStack.setCurrentWidget(self.ui.memberRegularEditPage)
+    def show_benchEdit(self):
+        self.ui.mainStack.setCurrentWidget(self.ui.memberBenchPage)
+    # gameEditPage
+    def create_new_record(self):
+        #TODO: 創建新的比賽檔案
+        self.show_gameEdit()
+    def show_gameEdit(self):
+        self.ui.mainStack.setCurrentWidget(self.ui.gameEditPage)
