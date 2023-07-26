88faed40b2ba92ec569f6aa41c4553f85a2f3927
andrewhb614 <andrew50412@gmail.com>
2023-07-27 01:55:16 +0800

    修改 RecordBtnSet 引入 typeArr

--- src/components/recordPage/RecordBtnSet.jsx
+++ src/components/recordPage/RecordBtnSet.jsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 
 import RecordBtn from "./RecordBtn";
+import { typeArr } from "../../utils/playTypeArr";
 
 const Container = styled.div`
 	/* width: 100%;
@@ -12,69 +13,6 @@ const Container = styled.div`
 	row-gap: 1rem;
 	column-gap: 1rem;
 `;
-
-const typeArr = [
-	{
-		text: "發球得分",
-		type: "service",
-		win: true,
-		num: 0,
-	},
-	{
-		text: "發球失誤",
-		type: "service",
-		win: false,
-		num: 1,
-	},
-	{
-		text: "攻擊得分",
-		type: "attack",
-		win: true,
-		num: 2,
-	},
-	{
-		text: "攻擊失誤",
-		type: "attack",
-		win: false,
-		num: 3,
-	},
-	{
-		text: "攔網得分",
-		type: "block",
-		win: true,
-		num: 4,
-	},
-	{
-		text: "攔網失誤",
-		type: "block",
-		win: false,
-		num: 5,
-	},
-	{
-		text: "接發失誤",
-		type: "receive",
-		win: false,
-		num: 6,
-	},
-	{
-		text: "防守失誤",
-		type: "defense",
-		win: false,
-		num: 7,
-	},
-	{
-		text: "二傳失誤",
-		type: "set",
-		win: false,
-		num: 8,
-	},
-	{
-		text: "犯規",
-		type: "fault",
-		win: false,
-		num: 9,
-	}
-];
 
 const RecordBtnSet = () => {
 	return (
