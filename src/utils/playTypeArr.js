13208646525d35e250d8569f17149b31e3f16015
andrewhb614 <andrew50412@gmail.com>
2023-07-27 01:49:04 +0800

    新增 typeArr

---
+++ src/utils/playTypeArr.js
@@ -0,0 +1,62 @@
+export const typeArr = [
+	{
+		text: "發球得分",
+		type: "service",
+		win: true,
+		num: 0,
+	},
+	{
+		text: "發球失誤",
+		type: "service",
+		win: false,
+		num: 1,
+	},
+	{
+		text: "攔網得分",
+		type: "block",
+		win: true,
+		num: 2,
+	},
+	{
+		text: "攔網失誤",
+		type: "block",
+		win: false,
+		num: 3,
+	},
+	{
+		text: "攻擊得分",
+		type: "attack",
+		win: true,
+		num: 4,
+	},
+	{
+		text: "攻擊失誤",
+		type: "attack",
+		win: false,
+		num: 5,
+	},
+	{
+		text: "接發失誤",
+		type: "receive",
+		win: false,
+		num: 6,
+	},
+	{
+		text: "防守失誤",
+		type: "defense",
+		win: false,
+		num: 7,
+	},
+	{
+		text: "二傳失誤",
+		type: "set",
+		win: false,
+		num: 8,
+	},
+	{
+		text: "犯規",
+		type: "fault",
+		win: false,
+		num: 9,
+	},
+];
