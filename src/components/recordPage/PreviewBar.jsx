7c045604d34255924b4ad284489e2c73bc875bd5
andrewhb614 <andrew50412@gmail.com>
2023-08-01 17:59:59 +0800

    修改 PreviewBar 排版, 新增 numberFormatter 功能

--- src/components/recordPage/PreviewBar.jsx
+++ src/components/recordPage/PreviewBar.jsx
@@ -2,26 +2,42 @@ import { useSelector } from "react-redux";
 import styled from "@emotion/styled";
 
 import { recordTypeArr } from "../../utils/recordTypeArr";
+import numberFormatter from "../../utils/numberFormatter";
 
 const Container = styled.div`
 	flex: 1 1;
-	align-items: center;
+	padding: 0 1rem;
 	border: 1px solid var(--black-primary);
 	border-radius: 1rem;
 	display: flex;
+	align-items: center;
+	gap: 0.5rem;
 `;
 
-const ScorePreview = styled.p`
-	/* FIXME: 使文字垂直置中 需再研究 目前只能單行 */
-	padding-left: 1rem;
-	font-size: 1.2rem;
-	display: inline-block;
+const ScorePreview = styled.div`
+	flex: 0 1 3.5rem;
+	font-size: 1.5rem;
+	font-weight: 700;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
 `;
 
-const ContentPreview = styled.p`
-	padding-left: 0.5rem;
-	font-size: 1.2rem;
-	display: inline-block;
+const Score = styled.div`
+	flex: 1 1 1.5rem;
+	font-size: 1.5rem;
+	font-weight: 700;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+`;
+
+const ContentPreview = styled.div`
+	font-size: 1.25rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 `;
 
 const PreviewBar = () => {
@@ -44,6 +60,9 @@ const PreviewBar = () => {
 			? member.number === lastPlay?.playerNum
 			: member.number === recordingPlay.playerNum;
 	});
+	const { name, number } = member || { name: "", number: "" };
+	const { scoreOurs, scoreOppo } =
+		recordingPlay.typeNum === -1 ? lastPlay : recordingPlay;
 	const playType =
 		recordingPlay.typeNum === -1
 			? recordTypeArr[lastPlay.typeNum]?.text
@@ -52,31 +71,22 @@ const PreviewBar = () => {
 	return (
 		<Container>
 			<ScorePreview>
-				{recordingPlay.typeNum === -1
-					? `${lastPlay.scoreOurs}-${lastPlay.scoreOppo}`
-					: `${recordingPlay.scoreOurs}-${recordingPlay.scoreOppo}`}
+				<Score>{scoreOurs}</Score>:<Score>{scoreOppo}</Score>
 			</ScorePreview>
 			<ContentPreview>
-				{
-					recordingPlay.playerNum === -1
-						? recordingPlay.typeNum === -1
-							? lastPlay.isNewSet
-								? ``
-								: lastPlay.playerNum === -1
-									? `${playType}`
-									: `${member.name}(${member.number}) ${playType}`
-							: `${playType}`
-						: recordingPlay.typeNum === -1
-							? `${member.name}(${member.number})`
-							: `${member.name}(${member.number}) ${playType}`
-					// recordingPlay.typeNum !== -1
-					// 	? `${member.name}(${member.number}) ${playType}`
-					// 	: recordingPlay.playerNum !== -1
-					// 	? `${member.name}(${member.number})` // p 有, t 沒有
-					// 	: lastPlay?.isNewSet // p 沒有, t 沒有
-					// 	? `` // 上球無紀錄內容
-					// 	: `${member.name}(${member.number}) ${playType}` // 上球有紀錄內容
-				}
+				{recordingPlay.playerNum === -1
+					? recordingPlay.typeNum === -1
+						? lastPlay.isNewSet
+							? ``
+							: lastPlay.playerNum === -1
+							? `${playType}`
+							: `${name.padStart(4, "　")}(${numberFormatter(
+									number
+							  )}) ${playType}`
+						: `${playType}`
+					: recordingPlay.typeNum === -1
+					? `${name.padStart(4, "　")}(${numberFormatter(number)})`
+					: `${name.padStart(4, "　")}(${numberFormatter(number)}) ${playType}`}
 			</ContentPreview>
 		</Container>
 	);
---
+++ src/utils/numberFormatter.js
@@ -0,0 +1,5 @@
+const numberFormatter = (num) => {
+	return num.toString().padStart(2, "0");
+};
+
+export default numberFormatter;
