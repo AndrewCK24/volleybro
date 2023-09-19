132583ded488d75b111c06f3e14083e1902f6e20
andrewhb614 <andrew50412@gmail.com>
2023-09-19 17:57:23 +0800

    [UI components] 開始編輯 Button 元件

--- src/components/general/Button/index.jsx
+++ src/components/general/Button/index.jsx
@@ -2,15 +2,40 @@ import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 import PropTypes from "prop-types";
 
+const containedStyle = css`
+  background-color: ${({ theme }) => theme.colors.secondary[4]};
+  color: ${({ theme }) => theme.colors.primary[0]};
+`;
+
+const outlinedStyle = css`
+  background-color: ${({ theme }) => theme.colors.primary[0]};
+  color: ${({ theme }) => theme.colors.secondary[4]};
+  border: 1px solid ${({ theme }) => theme.colors.secondary[4]};
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondary[4] + "10"};
+  }
+`;
+
+const textStyle = css`
+  background-color: transparent;
+  color: ${({ theme }) => theme.colors.secondary[4]};
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondary[4] + "10"};
+  }
+`;
+
+const disabledStyle = css`
+  cursor: not-allowed;
+  &:hover, &:active {
+    opacity: 1;
+  }
+`;
+
 const variantMap = {
   contained: containedStyle,
   outlined: outlinedStyle,
   text: textStyle,
 };
-
-const containedStyle = css`
-  background-color: ${({ theme }) => theme.colors.primary};
-`;
 
 const StyledButton = styled.button`
   display: flex;
