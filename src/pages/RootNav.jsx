2584fece9d927b57991ded9991c112a9b6f3136c
andrew <andrew50412@gmail.com>
2023-09-22 16:15:59 +0800

    [Root] 修改 StartRecordBtn 顯示時機

--- src/pages/RootNav.jsx
+++ src/pages/RootNav.jsx
@@ -1,4 +1,5 @@
 import { Container, PagesContainer } from "./Root";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
 import Header from "../components/root/Header";
@@ -6,13 +7,14 @@ import BottomNav from "../components/root/BottomNav";
 import StartRecordBtn from "../components/root/StartRecordBtn";
 
 const RootNavLayout = () => {
+  const teamId = useSelector((state) => state.team._id);
   return (
     <Container>
       <Header />
       <PagesContainer>
         <Outlet />
       </PagesContainer>
-      <StartRecordBtn />
+      {teamId && <StartRecordBtn />}
       <BottomNav />
     </Container>
   );
