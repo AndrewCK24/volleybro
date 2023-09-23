8bfedcc594f6b5b9f373a4d901f12e63aba304dc
andrew <andrew50412@gmail.com>
2023-09-23 14:19:53 +0800

    [USER] 修改 userSchema 新增 name 欄位

--- src/components/user/user-slice.js
+++ src/components/user/user-slice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   signIn: false,
+  name: "",
   email: "",
   teamIds: [],
   _id: "",
@@ -13,12 +14,14 @@ const userSlice = createSlice({
   reducers: {
     loadUserData(state, action) {
       state.signIn = true;
+      state.name = action.payload.name;
       state.email = action.payload.email;
       state.teamIds = action.payload.teamIds;
       state._id = action.payload._id;
     },
     signOut(state) {
       state.signIn = false;
+      state.name = "";
       state.email = "";
       state.teamIds = [];
       state._id = "";
