11781302cb2d4c721a94b918626914f1aecc3def
AndrewCK24 <andrew50412@gmail.com>
2023-09-25 19:19:56 +0800

    [Auth] 修改 AuthForm 使用 FormControl 重構

--- src/components/auth/AuthForm.jsx
+++ src/components/auth/AuthForm.jsx
@@ -1,35 +1,65 @@
+import { useState } from "react";
 import {
-  StyledForm,
-  StyledLabel,
-  StyledInput,
-  StyledButton,
+  FormContainer,
+  FormTitle,
+  FormContents,
+  FormControl,
+  FormButton,
   StyledLink,
 } from "../general/Form";
 
 const AuthForm = () => {
+  const [emailError, setEmailError] = useState(" ");
+  const [passwordError, setPasswordError] = useState(" ");
+  const errorArr = [emailError, passwordError];
+
+  const handleEmailChange = (value) => {
+    const validEmailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (value.length === 0) {
+      setEmailError("帳號不得為空");
+    } else if (!value.match(validEmailRegex)) {
+      setEmailError("請輸入有效的 email");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const handlePasswordChange = (value) => {
+    if (value.length === 0) {
+      setPasswordError("密碼不得為空");
+    } else if (value.length > 20) {
+      setPasswordError("密碼不得超過20字");
+    } else {
+      setPasswordError("");
+    }
+  };
+
   return (
-    <StyledForm method="post" action="/auth">
-      <StyledLabel htmlFor="email">帳號</StyledLabel>
-      <StyledInput
-        type="email"
-        placeholder="email"
-        id="email"
-        name="email"
-        required
-        autoComplete="on"
-      />
-      <StyledLabel htmlFor="password">密碼</StyledLabel>
-      <StyledInput
-        type="password"
-        placeholder="password"
-        id="password"
-        name="password"
-        required
-        autoComplete="on"
-      />
-      <StyledLink to="/auth/password">忘記密碼？</StyledLink>
-      <StyledButton type="submit">註冊 / 登入</StyledButton>
-    </StyledForm>
+    <FormContainer method="post" action="/auth">
+      <FormTitle>歡迎回來 V-Stats</FormTitle>
+      <FormContents>
+        <FormControl
+          name="email"
+          labelText="帳號"
+          type="email"
+          placeholder="請輸入 email"
+          required={true}
+          onChange={handleEmailChange}
+          warn={emailError}
+        />
+        <FormControl
+          name="password"
+          labelText="密碼"
+          type="password"
+          placeholder="請輸入密碼"
+          required={true}
+          onChange={handlePasswordChange}
+          warn={passwordError}
+        />
+        <StyledLink to="/auth/password">忘記密碼？</StyledLink>
+      </FormContents>
+      <FormButton errorArr={errorArr}>註冊 / 登入</FormButton>
+    </FormContainer>
   );
 };
 
