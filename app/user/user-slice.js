38c757e186ab0680f1b639e5cf6d5290b1222bbb
AndrewCK24 <andrew50412@gmail.com>
2023-10-31 16:12:52 +0800

    feat: remodel initial state of user slice

--- app/user/user-slice.js
+++ app/user/user-slice.js
@@ -10,24 +10,32 @@ const initialState = {
   },
   info: {},
   preferences: {},
+  _id: "",
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser(state, action) {
+    setUser: (state, action) => {
       state.signIn = true;
       state.name = action.payload.name;
       state.email = action.payload.email;
       state.teams = action.payload.teams;
+      state.info = action.payload.info;
+      state.preferences = action.payload.preferences;
       state._id = action.payload._id;
     },
-    signOut(state) {
+    signOut: (state) => {
       state.signIn = false;
       state.name = "";
       state.email = "";
-      state.teams = [];
+      state.teams = {
+        joined: [],
+        inviting: [],
+      };
+      state.info = {};
+      state.preferences = {};
       state._id = "";
     },
   },
