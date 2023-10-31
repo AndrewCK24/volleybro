7543e220fb7ca6308c603aa6d9c10c2eb01505d1
AndrewCK24 <andrew50412@gmail.com>
2023-10-31 14:31:41 +0800

    feat: POST /api/team create-team function

---
+++ app/api/team/route.js
@@ -0,0 +1,57 @@
+import { NextResponse } from "next/server";
+import verifyJwt from "../utils/verify-jwt";
+import signJwt from "../utils/sign-jwt";
+import hidePassword from "../utils/hide-password";
+import User from "@/app/models/user";
+import Team from "@/app/models/team";
+import Member from "@/app/models/member";
+
+export const POST = async (req) => {
+  try {
+    const { userData } = await verifyJwt(req);
+    const newMember = new Member({
+      name: userData.name,
+      meta: {
+        admin: true,
+        email: userData.email,
+        user_id: userData._id,
+      },
+    });
+
+    const { name, nickname } = await req.json();
+    // TODO: Add validation for name and nickname
+    // if there is a team with the same name, ask the user to choose another name or join the existing team
+    const newTeam = new Team({
+      name,
+      nickname,
+      members: [newMember._id],
+    });
+
+    const updatedUser = await User.findById(userData._id);
+    newMember.team_id = newTeam._id;
+    updatedUser.teams.joined = newTeam._id;
+
+    await newMember.save();
+    await newTeam.save();
+    await updatedUser.save();
+
+    const user = hidePassword(updatedUser);
+    const token = signJwt(user);
+    const response = NextResponse.json(
+      { userData: user, teamData: newTeam, membersData: [newMember] },
+      { status: 201 }
+    );
+    response.cookies.set({
+      name: "token",
+      value: token,
+      options: {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 30,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log("[create-team]", error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+};
