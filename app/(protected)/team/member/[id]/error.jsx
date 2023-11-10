14b9fa4f8e0becc982384da8562aad71f4c18a94
AndrewCK24 <andrew50412@gmail.com>
2023-11-11 02:53:36 +0800

    feat: add error page for team member

---
+++ app/(protected)/team/member/[id]/error.jsx
@@ -0,0 +1,12 @@
+"use client";
+import { Section } from "@/app/components/common/Section";
+
+const MemberError = () => {
+  return (
+    <Section>
+      <h1>Member Error</h1>
+    </Section>
+  );
+}
+
+export default MemberError;
