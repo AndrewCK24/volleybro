782652efd7f3f2bb0280afc02f356e2c081f4728
AndrewCK24 <andrew50412@gmail.com>
2024-05-02 22:15:07 +0800

    refactor(comp): 使用 tailwindcss 重構 Logo(#25)

--- app/(auth)/sign-in/page.jsx
+++ app/(auth)/sign-in/page.jsx
@@ -1,15 +1,11 @@
 import { Card } from "@/components/ui/card";
-import Logo from "@/app/components/common/Logo";
 import SignInForm from "./SignInForm";
 
 const SignInPage = () => {
   return (
-    <>
-      <Logo type="maximized" />
-      <Card className="w-full">
-        <SignInForm />
-      </Card>
-    </>
+    <Card className="w-full">
+      <SignInForm />
+    </Card>
   );
 };
 
--- app/components/common/Logo.jsx
+++ app/components/common/Logo.jsx
@@ -1,13 +1,7 @@
-import styles from "./styles.module.scss";
-
-const Logo = ({ type }) => (
-  <div
-    className={`${styles.logo} ${
-      type === "maximized" && styles["logo--maximized"]
-    }`}
-  >
-    V-STATS
+export const Logo = () => (
+  <div className="flex items-center justify-center flex-1 w-full">
+    <h1 className="m-0 text-[3rem] font-medium text-primary-foreground shadow-primary-foreground/50 drop-shadow-[0_0_0.25rem]">
+      V-STATS
+    </h1>
   </div>
 );
-
-export default Logo;
