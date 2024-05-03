5ca9940975afd9cff6afdf9022c6767c0e045d8c
AndrewCK24 <andrew50412@gmail.com>
2024-05-03 21:31:30 +0800

    refactor(user): 重構 User 頁面, 移除 Section (#25)

--- app/(protected)/user/Menu.jsx
+++ app/(protected)/user/Menu.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-
 import { userActions } from "../user/user-slice";
 import { teamActions } from "../team/team-slice";
 import {
@@ -13,11 +12,12 @@ import {
   FiUsers,
   FiUserPlus,
   FiPlus,
-  FiLogOut,
 } from "react-icons/fi";
 import { GoArrowSwitch } from "react-icons/go";
-import { Section, SectionHr } from "../../components/common/Section";
-import { ListItem, ListItemText, ListBtn } from "../../components/common/List";
+import { Button, Link } from "@/components/ui/button";
+import { CardDescription } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { ListItem, ListItemText } from "../../components/common/List";
 
 const ExtendTeamsIcon = styled(FiChevronDown)`
   transition: transform 0.2s ease-in-out;
@@ -67,85 +67,65 @@ const Menu = () => {
     }
   };
 
-  const handleSignOut = async () => {
-    try {
-      await fetch("/api/sign-out", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-      dispatch(userActions.signOut());
-      dispatch(teamActions.resetTeam());
-      router.push("/sign-in");
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <>
-      <Section>
-        <ListItem type="primary">
-          <FiUser />
-          <ListItemText>{userName}</ListItemText>
-        </ListItem>
-        <ListItem onClick={() => handleExtendTeams()}>
-          <FiUserPlus />
-          <ListItemText>隊伍與邀請</ListItemText>
-          <ListItemText minimized>{invitingTeams.length}</ListItemText>
-          <ExtendTeamsIcon className={extendTeams && "up"} />
-        </ListItem>
-        {extendTeams && (
-          <>
-            {joinedTeams.length > 0 && <SectionHr content="已加入隊伍" />}
-            {joinedTeams.map((team, index) => (
-              <ListItem
-                key={index}
-                type="primary"
-                text
-                onClick={() => handleTeamSwitch(index, team)}
-              >
-                <FiUsers />
-                <ListItemText>{team.name || ""}</ListItemText>
-                {index !== 0 && <GoArrowSwitch />}
-              </ListItem>
-            ))}
-            {invitingTeams.length > 0 && <SectionHr content="收到的邀請" />}
-            {invitingTeams.map((team, index) => (
-              <ListItem
-                key={index}
-                type="primary"
-                text
-                onClick={() => router.push(`/team/info/${team._id}`)}
-              >
-                <FiUsers />
-                <ListItemText>{team.name || ""}</ListItemText>
-              </ListItem>
-            ))}
-            <span>沒有你的隊伍嗎？你可以聯絡你的隊伍管理者，或...</span>
+      <Button size="wide">
+        <FiUser />
+        <ListItemText>{userName}</ListItemText>
+      </Button>
+      <ListItem onClick={() => handleExtendTeams()}>
+        <FiUserPlus />
+        <ListItemText>隊伍與邀請</ListItemText>
+        <ListItemText minimized>{invitingTeams.length}</ListItemText>
+        <ExtendTeamsIcon className={extendTeams && "up"} />
+      </ListItem>
+      {extendTeams && (
+        <>
+          {joinedTeams.length > 0 && (
+            <CardDescription>已加入隊伍</CardDescription>
+          )}
+          {joinedTeams.map((team, index) => (
             <ListItem
-              type="danger"
+              key={index}
+              type="primary"
               text
-              onClick={() => router.push("/team/new")}
+              onClick={() => handleTeamSwitch(index, team)}
             >
-              <FiPlus />
-              建立隊伍
+              <FiUsers />
+              <ListItemText>{team.name || ""}</ListItemText>
+              {index !== 0 && <GoArrowSwitch />}
             </ListItem>
-          </>
-        )}
-        <ListItem>
-          <FiSettings />
-          設定
-        </ListItem>
-      </Section>
-      <Section type="transparent">
-        <ListItem type="danger" center={true} onClick={handleSignOut}>
-          <FiLogOut />
-          登出
-        </ListItem>
-      </Section>
+          ))}
+          {invitingTeams.length > 0 && (
+            <>
+              <Separator />
+              <CardDescription>收到的邀請</CardDescription>
+            </>
+          )}
+          {invitingTeams.map((team, index) => (
+            <ListItem
+              key={index}
+              type="primary"
+              text
+              onClick={() => router.push(`/team/info/${team._id}`)}
+            >
+              <FiUsers />
+              <ListItemText>{team.name || ""}</ListItemText>
+            </ListItem>
+          ))}
+          <CardDescription>
+            沒有你的隊伍嗎？你可以聯絡你的隊伍管理者，或...
+          </CardDescription>
+          <Link variant="ghost" size="lg" href="/team/invitations">
+            <FiPlus />
+            查看更多
+          </Link>
+        </>
+      )}
+      <ListItem>
+        <FiSettings />
+        設定
+      </ListItem>
     </>
   );
 };
--- app/(protected)/user/page.jsx
+++ app/(protected)/user/page.jsx
@@ -1,8 +1,44 @@
 "use client";
+import { useDispatch } from "react-redux";
+import { userActions } from "../user/user-slice";
+import { teamActions } from "../team/team-slice";
+import { FiLogOut } from "react-icons/fi";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import Menu from "./Menu";
 
 const userPage = () => {
-  return <Menu />;
+  const dispatch = useDispatch();
+  const handleSignOut = async () => {
+    try {
+      await fetch("/api/sign-out", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      dispatch(userActions.signOut());
+      dispatch(teamActions.resetTeam());
+      router.push("/sign-in");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <Card className="w-full">
+        <Menu />
+      </Card>
+      <Card className="w-full py-0 bg-transparent shadow-none">
+        <Button variant="destructive" size="lg" onClick={handleSignOut}>
+          <FiLogOut />
+          登出
+        </Button>
+      </Card>
+    </>
+  );
 };
 
 export default userPage;
