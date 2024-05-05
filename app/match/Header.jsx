deba4d197c717d41a50b1bf091356da505302621
AndrewCK24 <andrew50412@gmail.com>
2024-05-06 03:38:53 +0800

    refactor(match): 使用 tailwind 重構 Header, Scores 元件(#25)

--- app/match/Header.jsx
+++ app/match/Header.jsx
@@ -1,53 +1,24 @@
 "use client";
-import styled from "styled-components";
 import { useRouter, useSelectedLayoutSegment } from "next/navigation";
 import { FiArrowLeft, FiSettings } from "react-icons/fi";
+import { Button } from "@/components/ui/button";
 import Scores from "./Scores";
 
-const Container = styled.section`
-  flex: 0 0;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  border-radius: 0 0 1.5rem 1.5rem;
-  padding: 0 0.5rem 0.5rem;
-  background-color: var(--primary-100);
-  box-shadow: 0 0 1rem var(--primary-300);
-  gap: 0.5rem;
-`;
+const Container = ({ children }) => {
+  return (
+    <section className="flex flex-row items-center justify-center flex-none w-full bg-background rounded-b-[0.5rem] px-2 gap-2 shadow">
+      {children}
+    </section>
+  );
+};
 
-const BtnContainer = styled.button`
-  flex: 0 0;
-  height: 100%;
-  min-width: 4rem;
-  padding: 0 1rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: none;
-  color: var(--primary-900);
-  background-color: transparent;
-  svg {
-    flex: 1;
-    width: 2rem;
-    height: 2rem;
-  }
-`;
-
-const MainPart = styled.div`
-  flex: 3 1;
-  min-height: 3rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 0.5rem;
-  color: var(--primary-900);
-  font-size: 2rem;
-  font-weight: 600;
-`;
+const Content = ({ children }) => {
+  return (
+    <div className="flex flex-row items-center justify-center flex-1 gap-2 min-h-[3rem] pb-2 text-[1.625rem] font-medium">
+      {children}
+    </div>
+  );
+};
 
 const Header = ({ matchId }) => {
   const router = useRouter();
@@ -68,10 +39,10 @@ const Header = ({ matchId }) => {
 
   return (
     <Container>
-      <BtnContainer onClick={() => handleBack()}>
+      <Button variant="ghost" size="icon" onClick={() => handleBack()}>
         <FiArrowLeft />
-      </BtnContainer>
-      <MainPart
+      </Button>
+      <Content
         onClick={() => isRecording && router.push(`/match/${matchId}/overview`)}
       >
         {segment === "new" ? (
@@ -85,13 +56,17 @@ const Header = ({ matchId }) => {
         ) : (
           ""
         )}
-      </MainPart>
+      </Content>
       {matchId === "new" || segment === "config" ? (
-        <BtnContainer />
+        <Button />
       ) : (
-        <BtnContainer onClick={() => router.push(`/match/${matchId}/config`)}>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => router.push(`/match/${matchId}/config`)}
+        >
           <FiSettings />
-        </BtnContainer>
+        </Button>
       )}
     </Container>
   );
