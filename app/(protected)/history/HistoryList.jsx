a0aadca96ac2841b8bffbf489633d38b585cdc4b
AndrewCK24 <andrew50412@gmail.com>
2024-05-07 22:08:35 +0800

    refactor(match): 使用 data-table 重構 HistoryList(#25)

--- app/(protected)/history/HistoryList.jsx
+++ app/(protected)/history/HistoryList.jsx
@@ -1,23 +1,90 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
-import { ListItem, ListItemText } from "@/app/components/common/List";
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const columns = [
+  {
+    accessorKey: "name",
+    header: "比賽名稱",
+    cell: () => <>比賽</>,
+    // cell: ({ row }) => <>{row.original.info.match.name || "比賽"}</>,
+  },
+];
+
+const DataTable = ({ columns, data }) => {
+  const router = useRouter();
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <Table className="text-lg">
+      <TableHeader>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              return (
+                <TableHead
+                  key={header.id}
+                  style={{ width: `${header.getSize()}px` }}
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </TableHead>
+              );
+            })}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}
+              onClick={() => router.push(`/match/${row.original}`)}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell colSpan={columns.length} className="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
 
 const HistoryList = () => {
-  const router = useRouter();
   const { matches } = useSelector((state) => state.team);
-  return (
-    <>
-      {matches.map((matchId) => (
-        <ListItem
-          key={matchId}
-          onClick={() => router.push(`/match/${matchId}`)}
-        >
-          <ListItemText>比賽</ListItemText>
-        </ListItem>
-      ))}
-    </>
-  );
+  return <DataTable columns={columns} data={matches} />;
 };
 
 export default HistoryList;
