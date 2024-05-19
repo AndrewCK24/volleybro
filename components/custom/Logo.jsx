b103e6f108c3fcd82931547c543a0a2a57555132
AndrewCK24 <andrew50412@gmail.com>
2024-05-20 04:29:41 +0800

    refactor(auth): 修正 Logo 元件優先屬性, 重構 AuthForm 卡片結構

--- components/custom/Logo.jsx
+++ components/custom/Logo.jsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
 
 export const Logo = () => (
-  <div className="flex items-center justify-center flex-1 w-full">
-    <Image src="/logo.svg" alt="v-stats" width={200} height={100} />
+  <div className="flex items-center justify-center flex-1 w-full h-auto">
+    <Image
+      src="/logo.svg"
+      alt="v-stats"
+      width={200}
+      height={50.39}
+      priority={true}
+    />
   </div>
 );
