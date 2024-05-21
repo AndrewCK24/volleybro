60b100d581153e251b776fca131418ee62c08472
AndrewCK24 <andrew50412@gmail.com>
2024-05-22 03:20:18 +0800

    feat(auth): 移除 credential login 相關功能
    變動內容：
    1. 移除 auth.config.js 中的 credential login 相關設定
    2. 修改 linkAccount 事件中查詢 user 資料的方式
    
    issue #49

--- auth.config.js
+++ auth.config.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
-import { compareSync } from "bcryptjs";
 import Google from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials";
 import connectToMongoDB from "@/lib/connect-to-mongodb";
 
 const authConfig = {
@@ -10,51 +8,13 @@ const authConfig = {
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
-    Credentials({
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        let user = null;
-
-        // // logic to verify if user exists
-        await connectToMongoDB();
-        user = await mongoose.models.User.findOne({
-          email: credentials.email,
-        });
-        if (!user || !credentials.password) return null;
-
-        // logic to compare password
-        const isPasswordCorrect = compareSync(
-          credentials.password,
-          user.password
-        );
-        if (!isPasswordCorrect) return null;
-
-        // return user object with the their profile data
-        return user;
-      },
-    }),
   ],
   events: {
-    // signIn: async ({ user, account }) => {
-    //   // Allow OAuth providers to sign in without email verification
-    //   if (account.provider === "credentials") {
-    //     await connectToMongoDB();
-    //     const existingUser = await mongoose.models.User.findById(user._id);
-    //     if (!existingUser?.emailVerified) return false;
-    //   }
-    //   return true;
-    // },
     linkAccount: async ({ user }) => {
       await connectToMongoDB();
-      await mongoose.models.User.findOneAndUpdate(
-        { email: user.email },
-        { $set: { emailVerified: new Date() } }
-      );
+      await mongoose.models.User.findByIdAndUpdate(user._id, {
+        $set: { emailVerified: new Date() },
+      });
     },
   },
 };
