f2bc66db757af66026341f3bf11ed0fc48c18525
AndrewCK24 <andrew50412@gmail.com>
2024-06-19 23:19:19 +0800

    refactor: 移除 `user-slice` (已使用 `useUser` 取代)

--- app/store/store.js
+++ app/store/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import userReducer from "../(protected)/user/user-slice";
 import lineupsReducer from "@/app/store/lineups-slice";
 import matchReducer from "../match/match-slice";
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
     lineups: lineupsReducer,
     match: matchReducer,
   },
