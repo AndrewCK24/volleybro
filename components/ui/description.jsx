9d03801337b7d444464396158fcc33350cb8916f
AndrewCK24 <andrew50412@gmail.com>
2024-06-21 22:27:00 +0800

    fix: 修正 `description` 元件未傳送 Ref 的錯誤

--- components/ui/description.jsx
+++ components/ui/description.jsx
@@ -1,20 +1,36 @@
-export const Description = ({ startIcon, endIcon, children, ...props }) => {
+import * as React from "react";
+
+export const Description = React.forwardRef(
+  ({ startIcon, endIcon, children, ...props }, ref) => {
+    return (
+      <div
+        className="flex flex-row [&>svg]:w-6 [&>svg]:h-6 gap-2 items-center basis-10 min-h-10"
+        ref={ref}
+        {...props}
+      >
+        {startIcon}
+        <div className="flex flex-col flex-1">{children}</div>
+        {endIcon}
+      </div>
+    );
+  }
+);
+Description.displayName = "Description";
+
+export const DescriptionTitle = React.forwardRef(({ children }, ref) => {
   return (
-    <div
-      className="flex flex-row [&>svg]:w-6 [&>svg]:h-6 gap-2 items-center basis-10 min-h-10"
-      {...props}
-    >
-      {startIcon}
-      <div className="flex flex-col flex-1">{children}</div>
-      {endIcon}
-    </div>
+    <p className="font-semibold" ref={ref}>
+      {children}
+    </p>
   );
-};
+});
+DescriptionTitle.displayName = "DescriptionTitle";
 
-export const DescriptionTitle = ({ children }) => {
-  return <p className="font-semibold">{children}</p>;
-};
-
-export const DescriptionContent = ({ children }) => {
-  return <p className="text-muted-foreground">{children}</p>;
-};
+export const DescriptionContent = React.forwardRef(({ children }, ref) => {
+  return (
+    <p className="text-muted-foreground" ref={ref}>
+      {children}
+    </p>
+  );
+});
+DescriptionContent.displayName = "DescriptionContent";
