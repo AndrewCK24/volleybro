79ef2ecbc999dbf291ac6a38b96fc25eca2bff3a
AndrewCK24 <andrew50412@gmail.com>
2024-07-17 21:56:45 +0800

    refactor: Rename filename of `Logo` component

--- components/custom/Logo.jsx
+++ components/custom/Logo.jsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
-export const Logo = () => (
-  <div className="flex items-center justify-center flex-1 w-full h-auto">
+export const Logo = ({ className }) => (
+  <div
+    className={cn(
+      "flex items-center justify-center flex-1 w-full h-auto",
+      className
+    )}
+  >
     <Image
       src="/logo.svg"
       alt="VolleyBro"
