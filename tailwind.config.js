69d01a5848c74cae88b301909c0f9038bc58e145
AndrewCK24 <andrew50412@gmail.com>
2024-09-26 01:37:40 +0800

    fix: `tailwind.config` error due to excluding ts files from contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ const plugin = require("tailwindcss/plugin");
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}",
-    "./app/**/*.{js,jsx}",
-    "./src/**/*.{js,jsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   prefix: "",
   theme: {
