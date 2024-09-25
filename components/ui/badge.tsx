0ed662f28e17bd071574409ebe6e13f887eec02d
AndrewCK24 <andrew50412@gmail.com>
2024-09-25 21:18:13 +0800

    refactor: convert `badge` component to typescript

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,10 +1,10 @@
-import * as React from "react";
-import { cva } from "class-variance-authority";
+import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-md border px-2 py-0.5 text-sm font-semibold gap-1 transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 svg-[1rem]",
+  "inline-flex items-center rounded-md border px-2 py-0.5 text-sm font-semibold gap-1 transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 [&>svg]:w-4 [&>svg]:h-4",
   {
     variants: {
       variant: {
@@ -21,12 +21,16 @@ const badgeVariants = cva(
       variant: "default",
     },
   }
-);
+)
 
-function Badge({ className, variant, ...props }) {
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
   return (
     <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  );
+  )
 }
 
-export { Badge, badgeVariants };
+export { Badge, badgeVariants }
