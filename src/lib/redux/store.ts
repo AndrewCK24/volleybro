cdc8bd88eab13c3dba5a14458a8839b369e56bfa
AndrewCK24 <andrew50412@gmail.com>
2024-09-30 14:45:27 +0800

    refactor: convert `record-slice` to typescript

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import lineupsReducer from "@/src/app/store/lineups-slice";
-import recordReducer from "@/src/app/store/record-slice";
+import recordReducer from "@/src/lib/features/record/record-slice";
 import editingReducer from "@/src/app/store/editing-slice";
 
 export const makeStore = () => {
