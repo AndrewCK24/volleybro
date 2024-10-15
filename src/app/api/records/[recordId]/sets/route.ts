106c38ccbc51a42f73d19ccaefc77ca347a839b7
AndrewCK24 <andrew50412@gmail.com>
2024-10-15 22:03:22 +0800

    feat: add `update-set` feature

--- src/app/api/records/[recordId]/sets/route.ts
+++ src/app/api/records/[recordId]/sets/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createSetController } from "@/interface/controllers/record/create-set.controller";
+import { updateSetController } from "@/interface/controllers/record/update-set.controller";
 
 export const POST = async (
   req: NextRequest,
@@ -24,6 +25,32 @@ export const POST = async (
     return NextResponse.json(record, { status: 201 });
   } catch (error) {
     console.log("[CREATE SET]", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
+
+export const PUT = async (
+  req: NextRequest,
+  { params }: { params: { recordId: string } }
+) => {
+  try {
+    const request = await req.json();
+    const { recordId } = params;
+    const searchParams = req.nextUrl.searchParams;
+    const setIndex = parseInt(searchParams.get("si") || "0", 10);
+
+    const input = {
+      params: { recordId, setIndex },
+      data: {
+        options: request.options,
+      },
+    };
+
+    const record = await updateSetController(input);
+
+    return NextResponse.json(record, { status: 200 });
+  } catch (error) {
+    console.log("[UPDATE SET]", error);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 };
