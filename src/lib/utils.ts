7da2cc854da49c4f28f6e8dc725472370e165b0b
AndrewCK24 <andrew50412@gmail.com>
2024-10-17 15:59:58 +0800

    fix: type error of `cn` in `utils.ts`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: string[]) {
+export function cn(...inputs: any[]) {
   return twMerge(clsx(inputs));
 }
