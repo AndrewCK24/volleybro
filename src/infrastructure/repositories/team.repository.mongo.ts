7565ec26029d780a6e1fa14f824a448cba40deb8
AndrewCK24 <andrew50412@gmail.com>
2025-03-03 17:08:13 +0800

    refactor(infrastructure): improve mongoose schemas with TypeScript best practices
    - Use proper type annotations for all schema properties
    - Add explicit return types to model exports
    - Create BaseMongoRepository for shared repository functionality
    - Add type conversion methods in repository implementations

--- src/infrastructure/repositories/team.repository.mongo.ts
+++ src/infrastructure/repositories/team.repository.mongo.ts
@@ -1,25 +1,16 @@
 import { ITeamRepository } from "@/applications/repositories/team.repository.interface";
-import { Team } from "@/infrastructure/mongoose/schemas/team";
+import {
+  Team as TeamModel,
+  TeamDocument,
+} from "@/infrastructure/mongoose/schemas/team";
+import { Team } from "@/entities/team";
+import { BaseMongoRepository } from "@/infrastructure/repositories";
 
-export class TeamRepositoryImpl implements ITeamRepository {
-  async find(filter: { [key: string]: any }) {
-    return await Team.find(filter);
-  }
-
-  async findOne(filter: { [key: string]: any }) {
-    return await Team.findOne(filter);
-  }
-
-  async create(data: any) {
-    return await Team.create(data);
-  }
-
-  async update(filter: { [key: string]: any }, data: any) {
-    return await Team.findOneAndReplace(filter, data, { new: true });
-  }
-
-  async delete(filter: { [key: string]: any }) {
-    const result = await Team.findOneAndDelete(filter);
-    return !!result;
+export class TeamRepositoryImpl
+  extends BaseMongoRepository<Team, TeamDocument>
+  implements ITeamRepository
+{
+  constructor() {
+    super(TeamModel);
   }
 }
--- src/infrastructure/repositories/user.repository.mongo.ts
+++ src/infrastructure/repositories/user.repository.mongo.ts
@@ -1,25 +1,16 @@
 import { IUserRepository } from "@/applications/repositories/user.repository.interface";
-import { User } from "@/infrastructure/mongoose/schemas/user";
+import {
+  User as UserModel,
+  UserDocument,
+} from "@/infrastructure/mongoose/schemas/user";
+import { User } from "@/entities/user";
+import { BaseMongoRepository } from "@/infrastructure/repositories";
 
-export class UserRepositoryImpl implements IUserRepository {
-  async find(filter: { [key: string]: any }) {
-    return await User.find(filter);
-  }
-
-  async findOne(filter: { [key: string]: any }) {
-    return await User.findOne(filter);
-  }
-
-  async create(data: any) {
-    return await User.create(data);
-  }
-
-  async update(filter: { [key: string]: any }, data: any) {
-    return await User.findOneAndReplace(filter, data, { new: true });
-  }
-
-  async delete(filter: { [key: string]: any }) {
-    const result = await User.findByIdAndDelete(filter);
-    return !!result;
+export class UserRepositoryImpl
+  extends BaseMongoRepository<User, UserDocument>
+  implements IUserRepository
+{
+  constructor() {
+    super(UserModel);
   }
 }
